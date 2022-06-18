967c95fccb0ad6a0edeb549133a13790d368bf0c
Alexis Quintero <alesete789@gmail.com>
2022-06-18 15:48:03 -0300

    se redujo las clases de container y se agrego una imagen

--- descuentos.js
+++ descuentos.js
@@ -16,12 +16,14 @@ function PriceDiscount(){
  const InputPrice = document.getElementById("inputPrice");
  const pricevalue = parseFloat(InputPrice.value);
 
- const InputDiscount = document.getElementById("inputDiscount");
+ const InputDiscount = document.getElementById("envio");
  const Discountvalue = parseFloat(InputDiscount.value);
-
+ 
  const precioFinal = calcularPrecioConDescuento(pricevalue, Discountvalue);
 //  para que al momento de regresar un resultado te lo escriba sobre la pagina web utilizaremos .innerText para devolver el resultado de de la operacion anterior sin que sea molesto para el usuario.
 
 const resultP = document.getElementById("ResultP");
+const resultP2 = document.getElementById("ResultP2")
 resultP.innerText ="El precio con descuento son $" + precioFinal
+resultP2.innerText = " se desconto un "+ Discountvalue + "%" 
 }
